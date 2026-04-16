af6011d2eb4630335c8c6cfbe6bb21665cb41142
Raj Kambar <rkambar2@gmail.com.com>
2026-04-16 22:32:27 +0530

    Initial commit

---
+++ src/main/java/com/cloudspring/cloudspring_intelligence/CloudspringIntelligenceApplication.java
@@ -0,0 +1,13 @@
+package com.cloudspring.cloudspring_intelligence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudspringIntelligenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudspringIntelligenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudspring/cloudspring_intelligence/CloudspringIntelligenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudspring.cloudspring_intelligence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudspringIntelligenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
